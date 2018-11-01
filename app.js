9877d7ae8ece971c695dc82bd783c77a4ed89a21
isabellebidou <isa.bidou@gmail.com>
2018-11-01 22:54:23 +0000

    added views

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static("scripts"));
 app.use(express.static("images"));
 app.set('view engine', 'jade');
 const db = mysql.createConnection({
-    host: ‘***********',
+    host: ‘********.com',
     user: ‘******',
     password: ‘******',
     database: ‘******',
@@ -42,12 +42,24 @@ app.get('/classes', function(req, res) {
         if (err) throw err;
         console.log(res1);
 
-        res.render('classes.jade', { root: VIEWS, res1 });
+        res.render('classes', { root: VIEWS, res1 });
     });
     console.log('now you are on classes');
 });
+//students page
+app.get('/students', function(req, res) {
 
-//createmuscle page
+    let sql = 'select * FROM danceclassusers INNER JOIN userRoles ON danceclassusers.userRoleId = userRoles.userRolesId WHERE danceclassusers.userRoleId = "student"; '
+    let query = db.query(sql, (err, res1) => {
+        if (err) throw err;
+        console.log(res1);
+
+        res.render('students', { root: VIEWS, res1 });
+    });
+    console.log('now you are on students');
+});
+
+//createclass page
 app.get('/createclass', function(req, res) {
     
         // res.sendFile('index.html', {root: VIEWS},behaviour);
@@ -56,9 +68,143 @@ app.get('/createclass', function(req, res) {
 
 });
 
-app.get('/createstudentstable', function(req, res) {
+//register page
+app.get('/register', function(req, res) {
+    
+        // res.sendFile('index.html', {root: VIEWS},behaviour);
+        res.render('register', { root: VIEWS });
+        console.log('now you ready to register');
 
-    let sql = 'CREATE TABLE classes (Id int NOT NULL AUTO_INCREMENT PRIMARY KEY, FName varchar(255), LName varchar(255), DateJoined date,  Price varchar(255), Comments varchar(255), Link varchar(255)) ;'
+});
+
+app.get('/createuserrolestable', function(req, res) {
+
+    let sql = 'CREATE TABLE userRoles (userRolesId int NOT NULL AUTO_INCREMENT PRIMARY KEY, userRolesName varchar(255));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+
+app.get('/createuserstable', function(req, res) {
+
+    let sql = 'CREATE TABLE danceclassusers (userId int NOT NULL AUTO_INCREMENT PRIMARY KEY, userFirstName varchar(255), userLastName varchar(255), userDateJoined date,  userComments varchar(255),userEmail varchar(50), userPassword varchar(12),userRole varchar(25),userActive boolean, FOREIGN KEY (userRoleId) REFERENCES userRoles(userRoleId));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+
+app.get('/createclassestable', function(req, res) {
+
+    let sql = 'CREATE TABLE classes (classId int NOT NULL AUTO_INCREMENT PRIMARY KEY, className varchar(255), classDate date, classVenue varchar(255), classTime varchar (255), classPrice varchar(255), classComments varchar(255), Link varchar(255));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+
+app.get('/createcarttable', function(req, res) {
+
+    let sql = 'CREATE TABLE cart (cartId int NOT NULL AUTO_INCREMENT PRIMARY KEY, cartDate date, cartStatus varchar(255), userId int (255),FOREIGN KEY (userId) REFERENCES danceclassusers(userId));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createcartclasstable', function(req, res) {
+
+    let sql = 'CREATE TABLE cartClass (cartId int, classId int,PRIMARY KEY (cartId, classId), FOREIGN KEY (cartId) REFERENCES cart(cartId),FOREIGN KEY (classId) REFERENCES classes(classId));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createuserclasstable', function(req, res) {
+
+    let sql = 'CREATE TABLE userClass (userId int, classId int,PRIMARY KEY (userId, classId), FOREIGN KEY (userId) REFERENCES danceclassusers(userId),FOREIGN KEY (classId) REFERENCES class(classId));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createstepfamilytable', function(req, res) {
+
+    let sql = 'CREATE TABLE stepFamily (stepFamilyId int NOT NULL AUTO_INCREMENT PRIMARY KEY, stepFamilyName varchar(255));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createstepcategorytable', function(req, res) {
+
+    let sql = 'CREATE TABLE stepCategory (stepCategoryId int NOT NULL AUTO_INCREMENT PRIMARY KEY, stepCategoryName varchar(255));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createstepzillpatterntable', function(req, res) {
+
+    let sql = 'CREATE TABLE stepZillPattern (stepZillPatternId int NOT NULL AUTO_INCREMENT PRIMARY KEY, stepZillPatternName varchar(255));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+
+app.get('/createsteptable', function(req, res) {
+
+    let sql = 'CREATE TABLE step (stepId int NOT NULL AUTO_INCREMENT PRIMARY KEY, stepName varchar(255), stepFamilyId varchar(255), stepCategoryId varchar(255),  stepZillPatternId varchar(255),  stepComments varchar(255), stepLink varchar(255),FOREIGN KEY (stepFamilyId) REFERENCES stepFamily(stepFamilyId),FOREIGN KEY (stepCategoryId) REFERENCES stepCategory(stepCategoryId),FOREIGN KEY (stepZillPatternId) REFERENCES stepZillPattern(stepZillPatternId));'
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createtable', function(req, res) {
+
+    let sql = ''
+
+
+    let query = db.query(sql, (err, res) => {
+        if (err) throw err;
+
+
+    });
+});
+app.get('/createclasssteptable', function(req, res) {
+
+    let sql = 'CREATE TABLE classStep (classId int, stepId int,PRIMARY KEY ( classId,stepId), FOREIGN KEY (classId) REFERENCES classes(classId),FOREIGN KEY (stepId) REFERENCES step(stepId));'
 
 
     let query = db.query(sql, (err, res) => {
@@ -141,4 +287,4 @@ app.get('/deleteclass/:id', function(req, res) {
 //set up the environment for the app to run
 app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
     console.log("app is running");
-})
+});
